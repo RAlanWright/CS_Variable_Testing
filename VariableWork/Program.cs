8807ce330f99770e821ac0f2e5453434d99f971d
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-04 14:05:48 -0500

    Added files to Visual Studio github extension

---
+++ VariableWork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was {0} years old", characterAge);
+            Console.WriteLine("He really liked the name {0}", characterName);
+            Console.WriteLine("But didn't like being {0} years old", characterAge);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
